51a444174212724220ad149ae7341dce703da690
Daniel López <70321598+dlopez-bioinfo@users.noreply.github.com>
2022-10-07 09:08:35 +0200

    added package versions

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     packages=['sptest'],
     include_package_data=True,
     install_requires=[
-        'numpy', 'pandas', 'click', 'scikit-learn',
-        'matplotlib', 'hyperopt', 'xgboost', 'joblib'
+        'numpy==1.16.2', 'pandas==0.24.2', 'click==7.0', 'scikit-learn==0.20.3',
+        'matplotlib==3.0.3', 'hyperopt==0.1.2', 'xgboost==0.90', 'joblib==0.13.2'
     ],
     entry_points={
         'console_scripts': ['sptest = sptest.cli:main']
